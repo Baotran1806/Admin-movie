74851d04076dee11eb63c591db95a08454c4835c
Baotran201806 <1851050153tran@ou.edu.vn>
2022-09-25 16:25:28 +0700

    done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from './redux/configStore';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+
+//antd
+import "antd/dist/antd.css";
+
+
+
+
+//Đoạn code để kết nối đến server lắng nghe sự kiện từ server
+// export const connection = new signalR.HubConnectionBuilder().withUrl(`${DOMAIN}/DatVeHub`).configureLogging(signalR.LogLevel.Information).build();
+
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
+  ,
+  document.getElementById('root')
 );
+
+// connection.start().then(() => {
+//   ReactDOM.render(
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//     ,
+//     document.getElementById('root')
+//   );
+// }).catch(errors => {
+//   console.log(errors);
+// })
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
